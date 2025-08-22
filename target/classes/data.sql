2d22730b78d749b4e7bc32826b600f253c4a70f3
eet43 <1998opening@gmail.com>
2025-08-22 16:37:46 +0900

    chore: 불필요한 파일 제거

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO events (name, total_tickets, available_tickets, event_date, created_at, updated_at) 
-VALUES 
-    ('Spring Conference 2024', 100, 100, '2024-12-31 18:00:00', NOW(), NOW()),
-    ('Java User Group Meetup', 50, 50, '2024-12-25 14:00:00', NOW(), NOW()),
-    ('Tech Talk: Distributed Systems', 30, 30, '2024-12-20 19:00:00', NOW(), NOW());
